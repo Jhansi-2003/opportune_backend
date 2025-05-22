c547e13dcdd81f79f08d8a6224342cb25ce51ab5
jhansi <jhansinarasingula25@gmail.com>
2025-05-22 08:04:17 +0530

    register route fix

--- server.js
+++ server.js
@@ -210,7 +210,7 @@ app.post("/api/reset-password", [
 });
 
 // Registration Route
-app.post("/register", [
+app.post("/api/register", [
   body("username").notEmpty().withMessage("Username is required"),
   body("email").isEmail().withMessage("Valid email is required"),
   body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
